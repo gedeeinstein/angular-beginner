85851389aa4f7819d72987d8085aed8327ef8335
gedeeinstein <gedeeinstein@gmail.com>
2020-10-10 02:47:05 +0800

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,6 +31,10 @@ export class AppModule { }
 
 
 /*
+
+lesson until this page 
+https://angular.io/start/start-data
+
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
